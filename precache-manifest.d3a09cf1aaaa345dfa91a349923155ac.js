acb402fb6f4190c203199ae91b87b448cb98f423
Guess <guess@Guesss-iMac.local>
2020-03-06 13:28:15 -0500

    Updates

---
+++ precache-manifest.d3a09cf1aaaa345dfa91a349923155ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3d7b1ed3b35f973c24c009570a1fb7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7f65a2f358739347010f",
+    "url": "/portfolio/static/css/main.c43809df.chunk.css"
+  },
+  {
+    "revision": "154050aa99c8e55df35e",
+    "url": "/portfolio/static/js/2.ad247420.chunk.js"
+  },
+  {
+    "revision": "cfae5b7d83e903e3eac5d300e4eb8849",
+    "url": "/portfolio/static/js/2.ad247420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f65a2f358739347010f",
+    "url": "/portfolio/static/js/main.00f37676.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  }
+]);
